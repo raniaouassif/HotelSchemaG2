24efcd7b50ec3422a61b266875cd9c92b58c0158
lupitaperalta <89276351+lupitaperalta@users.noreply.github.com>
2023-06-20 12:36:20 -0500

    Update G2-HotelData.sql

--- G2-HotelData.sql
+++ G2-HotelData.sql
@@ -6,9 +6,8 @@ CREATE DATABASE G2HotelDB;
 -- Make sure we're in the correct database before we add schema.
 USE G2HotelDB;
 
--- Creating the tables
 CREATE TABLE Guest (
-	GuestId INT PRIMARY KEY AUTO_INCREMENT,
+	guestId INT PRIMARY KEY AUTO_INCREMENT,
     `Name` VARCHAR(100) NOT NULL,
     Address VARCHAR(200) NOT NULL,
     City VARCHAR(50) NOT NULL,
@@ -17,65 +16,27 @@ CREATE TABLE Guest (
     PhoneNumber CHAR(14) NOT NULL
 );
 
-
--- Populate our database
-INSERT INTO Guest (GuestId, `Name`, Address, City, State, Zip, PhoneNumber) VALUES
-    (1, 'Mack Simmer', '379 Old Shore Street', 'Council Bluffs', 'IA', '51501', '(291) 553-0508'),
-    (2, 'Bettyann Seery', '750 Wintergreen Dr.', 'Wasilla', 'AK', '99654', '(478) 277-9632'),
-    (3, 'Duane Cullison', '9662 Foxrun Lane', 'Harlingen', 'TX', '78552', '(308) 494-0198'),
-    (4, 'Karie Yang', '9378 W. Augusta Ave.', 'West Deptford', 'NJ', '8096', '(214) 730-0298'),
-    (5, 'Aurore Lipton', '762 Wild Rose Street', 'Saginaw', 'MI', '48601', '(377) 507-0974'),
-    (6, 'Zachery Luechtefeld', '7 Poplar Dr.', 'Arvada', 'CO', '80003', '(814) 485-2615'),
-    (7, 'Jeremiah Pendergrass', '70 Oakwood St.', 'Zion', 'IL', '60099', '(279) 491-0960'),
-    (8, 'Walter Holaway', '7556 Arrowhead St.', 'Cumberland', 'RI', '2864', '(446) 396-6785'),
-    (9, 'Wilfred Vise', '77 West Surrey Street', 'Oswego', 'NY', '13126', '(834) 727-1001'),
-    (10, 'Maritza Tilton', '939 Linda Rd.', 'Burke', 'VA', '22015', '(446) 351-6860'),
-    (11, 'Joleen Tison', '87 Queen St.', 'Drexel Hill', 'PA', '19026', '(231) 893-2755');
-
--- Room type table and data
-CREATE TABLE roomType (
+CREATE TABLE RoomType (
 	roomTypeId INT NOT NULL,
     CONSTRAINT pk_roomType
 		PRIMARY KEY (roomTypeId),
 	roomType VARCHAR(50) NOT NULL,
     standardOccupancy INT NOT NULL,
     maxOccupancy INT NOT NULL,
-    basePrice BIGINT NOT NULL,
-    extraAdultFee BIGINT NOT NULL
+    basePrice DECIMAL NOT NULL,
+    extraAdultFee DECIMAL NOT NULL
 );
 
-INSERT INTO roomType (roomTypeId, RoomType, StandardOccupancy, MaxOccupancy, BasePrice, ExtraAdultFee) VALUES
-	(2, 'Double', 2, 4, 199.99, 10),
-    (2, 'Double', 2, 4, 174.99, 10),
-    (2, 'Double', 2, 4, 199.99, 10),
-    (2, 'Double', 2, 4, 174.99, 10),
-    (1, 'Single', 2, 2, 174.99, 0),
-    (1, 'Single', 2, 2, 149.99, 0),
-    (1, 'Single', 2, 2, 174.99, 0),
-    (1, 'Single', 2, 2, 149.99, 0),
-    (2, 'Double', 2, 4, 199.99, 10),
-    (2, 'Double', 2, 4, 174.99, 10),
-    (2, 'Double', 2, 4, 199.99, 10),
-    (2, 'Double', 2, 4, 174.99, 10),
-    (1, 'Single', 2, 2, 174.99, 0),
-    (1, 'Single', 2, 2, 149.99, 0),
-    (1, 'Single', 2, 2, 174.99, 0),
-    (1, 'Single', 2, 2, 149.99, 0),
-    (3, 'Suite', 3, 8, 399.99, 20),
-    (3, 'Suite', 3, 8, 399.99, 20); 
-
-DROP TABLE IF EXISTS RoomAmenity;
-CREATE TABLE RoomAmenity (
-    amenityID INT,
-    roomTypeID INT,
-    CONSTRAINT pk_RoomAmenity
-    	PRIMARY KEY (amenityID, roomTypeID),
-    CONSTRAINT fk_RoomAmenity_Amenity
-    	FOREIGN KEY (amenityID)
-    	REFERENCES Amenity(amenityID),
-    CONSTRAINT fk_pk_RoomAmenity_RoomType
-    	FOREIGN KEY (roomTypeID)
-    	REFERENCES RoomType(roomTypeID)
+CREATE TABLE Amenity (
+    amenityID INT PRIMARY KEY,
+    Room VARCHAR(10) NOT NULL,
+    Type VARCHAR(10) NOT NULL,
+    Amenities VARCHAR(100) NOT NULL,
+    ADAAccessible VARCHAR(3) NOT NULL,
+    StandardOccupancy INT NOT NULL,
+    MaximumOccupancy INT NOT NULL,
+    BasePrice DECIMAL NOT NULL,
+    ExtraPerson DECIMAL NOT NULL
 );
 
 CREATE TABLE Reservation (
@@ -90,6 +51,28 @@ CREATE TABLE Reservation (
         REFERENCES Guest(guestId)
 );
 
+CREATE TABLE Room (
+	roomNumber INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	adaAccessible BOOL NOT NULL,
+    roomTypeId INT NOT NULL,
+    TotalBase DECIMAL NOT NULL,
+    FOREIGN KEY fk_RoomType_Room (roomTypeId)
+		REFERENCES RoomType(roomTypeId)
+);
+
+CREATE TABLE RoomAmenity (
+    amenityID INT,
+    roomTypeID INT,
+    CONSTRAINT pk_RoomAmenity
+    	PRIMARY KEY (amenityID, roomTypeID),
+    CONSTRAINT fk_RoomAmenity_Amenity
+    	FOREIGN KEY (amenityID)
+    	REFERENCES Amenity(amenityID),
+    CONSTRAINT fk_pk_RoomAmenity_RoomType
+    	FOREIGN KEY (roomTypeID)
+    	REFERENCES RoomType(roomTypeID)
+);
+
 CREATE TABLE RoomReservation (
     roomNumber INT NOT NULL,
     reservation_id INT NOT NULL,
@@ -99,3 +82,6 @@ CREATE TABLE RoomReservation (
 	FOREIGN KEY fk_RoomReservation_Room (roomNumber)
         REFERENCES Room(roomNumber)	
 );
+
+
+
